efe655dd4cfd6a8d3ef39927b72a5275144ce2f4
Arul Mathur <arulster17@gmail.com>
2026-05-24 04:49:45 -0700

    get no chunked prefill data

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -8,13 +8,14 @@ def run(label, cmd):
     print(f"\n>>> {label}")
     subprocess.run(cmd, check=True)
 
-if len(sys.argv) != 4:
-    print("Usage: python scripts/run_all.py <ip> <port> <output_folder>")
+if len(sys.argv) != 5:
+    print("Usage: python scripts/run_all.py <ip> <port> <output_folder> <vllm-config>")
     sys.exit(1)
 
 ip  = sys.argv[1]
 port = sys.argv[2]
 output_folder = sys.argv[3]
+config = sys.argv[4]
 key = "~/.ssh/id_ed25519"
 
 ssh = ["ssh", "-t", f"root@{ip}", "-p", port, "-i", key]
@@ -26,7 +27,7 @@ run("Clearing old files on pod...",   ssh + ["rm -rf inference-lab"])
 run("Cloning repo on pod...",         ssh + ["git clone --branch 04-baseline https://github.com/arulster17/inference-lab.git"])
 run("Copying .env to pod...",         scp + [".env", f"root@{ip}:~/inference-lab/"])
 run("Installing dependencies...",     ssh + ["cd inference-lab && bash setup/install.sh"])
-run("Running experiment...",          ssh + [f"cd inference-lab && bash scripts/run_experiment.sh {output_folder}"])
+run("Running experiment...",          ssh + [f"cd inference-lab && bash scripts/run_experiment.sh {output_folder} {config}"])
 run("Copying results locally...",     scp + ["-r", f"root@{ip}:~/inference-lab/results/{output_folder}", f"results/{output_folder}"])
 run("Plotting...",                    ["python", "analysis/plot.py", "--results", f"results/{output_folder}"])
 
--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 OUTPUT_DIR=${1}
+CONFIG=${2
+
+}
 pkill -f "vllm serve" || true
-python serving/launch.py serving/configs/baseline.yaml
+python serving/launch.py $CONFIG
 bash scripts/run_concurrency.sh results/$OUTPUT_DIR
